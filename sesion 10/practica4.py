1a269647997fd3e750907603844e2065821d3439
ji798 <jisuazom@uamv.edu.ni>
2026-09-07 09:47:44 -0600

    Create practica4.py

---
+++ sesion 10/practica4.py
@@ -0,0 +1,8 @@
+lista_ventas = [100, 200, 300, 400, 500]
+
+def agregar_venta(venta):
+    lista_ventas.append(venta)
+    print("Venta agregada:", venta)
+
+agregar_venta(600)
+print("Lista de ventas:", lista_ventas)
