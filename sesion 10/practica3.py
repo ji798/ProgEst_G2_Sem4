e98667e7f96ddb6a3dfdd7ee6012714facfd973f
ji798 <jisuazom@uamv.edu.ni>
2026-09-07 09:47:35 -0600

    Create practica3.py

---
+++ sesion 10/practica3.py
@@ -0,0 +1,8 @@
+salario = 1000
+
+def aumentar_salario(salario):
+    salario += 500
+    print (f"nuevo salario: {salario}")
+
+aumentar_salario(salario)
+print(f"salario fuera de la función: {salario}")
