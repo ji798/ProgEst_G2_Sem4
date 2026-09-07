ba8c20b12b5f6e42b599ed46d845084bd6bd3927
ji798 <jisuazom@uamv.edu.ni>
2026-09-07 08:42:34 -0600

    Create error_varLocal.py

---
+++ sesion 10/error_varLocal.py
@@ -0,0 +1,5 @@
+def calcular_pago(horas, tarifa):
+    total = horas * tarifa
+
+print(f"El pago es: {total}")
+"""Ocurre un error porque la variable total solo existe dentro de la función calcular_pago."""
