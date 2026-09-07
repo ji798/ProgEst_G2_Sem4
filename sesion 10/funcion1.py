88d90ca7f5bb1f02ad4eb91f087599528b3bf635
ji798 <jisuazom@uamv.edu.ni>
2026-09-07 08:42:37 -0600

    Update funcion1.py

--- sesion 10/funcion1.py
+++ sesion 10/funcion1.py
@@ -7,3 +7,6 @@ calcular_pago(40, 120)
 
 # La siguiente instrucción produciría NameError:
 # print(pago)
+
+"""Si, el print(pago) da error porque la variable pago solo existe dentro de la función calcular_pago, 
+es decir, tiene un alcance local. Por lo tanto, no se puede acceder a ella desde fuera de la función."""
