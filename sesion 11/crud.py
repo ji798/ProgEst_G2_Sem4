6f348e0c71d57e8a7e89436bbfc693db9ef25e68
ji798 <jisuazom@uamv.edu.ni>
2026-09-09 07:49:20 -0600

    Update crud.py

---
+++ sesion 11/crud.py
@@ -0,0 +1,20 @@
+#Registro de estudiantes
+"""
+Registrar notas de n cantidad de estudiantes
+"""
+
+notas = []
+
+def agregar(nota):
+    notas.append(nota)
+
+def mostrar():
+    return notas
+
+def evaluarNotas():
+    for nota in notas: 
+        if nota >= 70:
+            print(f"{nota}, es aprobado")
+
+        else:
+            print(f"{nota}, tiene que mejorar.")
